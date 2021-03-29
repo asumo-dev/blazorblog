f123df904fb09f53583804f3f1827352a11e6174
asumo-dev <asumo.dev@gmail.com>
2021-03-29 20:36:55 +0900

    Add test for Contentful.BlogPostEntry

--- src/BlazorBlog.Contentful/BlogPostEntry.cs
+++ src/BlazorBlog.Contentful/BlogPostEntry.cs
@@ -5,25 +5,44 @@ using Contentful.Core.Models;
 
 namespace BlazorBlog.Contentful
 {
-    public class BlogPostEntry
+    public record BlogPostEntry
     {
-        public SystemProperties? Sys { get; set; } = default;
+        public SystemProperties? Sys { get; set; }
 
-        public string? Title { get; set; } = default;
+        public string? Title { get; set; }
 
-        public string? Slug { get; set; } = default;
+        public string? Slug { get; set; }
 
-        public Document? Body { get; set; } = default;
+        public Document? Body { get; set; }
 
-        public async Task<BlogPost> ToBlogPostAsync()
+        /// <summary>
+        /// Create a <see cref="BlogPost"/>
+        /// </summary>
+        /// <param name="converter">The instance of <see cref="IHtmlConverter"/> that is used to convert
+        /// <see cref="Document"/> to HTML. If this parameter is null <see cref="HtmlConverter"/> is used.</param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException">Cannot create a <see cref="BlogPost"/> from a
+        /// <see cref="BlogPostEntry"/> that any properties are null.</exception>
+        public async Task<BlogPost> ToBlogPostAsync(IHtmlConverter? converter = null)
         {
             if (Sys == null || Title == null || Slug == null || Body == null)
                 throw new InvalidOperationException(
                     "Entity fields cannot be null. Check your settings on Contentful website.");
-            
-            var htmlRenderer = new HtmlRenderer();
-            var html = await htmlRenderer.ToHtml(Body);
+
+            var html = await (converter ?? new HtmlConverter())
+                .ConvertDocumentToHtmlAsync(Body);
             return new BlogPost(Title, Slug, html, Sys.CreatedAt ?? default);
         }
     }
+
+    public interface IHtmlConverter
+    {
+        public Task<string> ConvertDocumentToHtmlAsync(Document document)
+        {
+            var htmlRenderer = new HtmlRenderer();
+            return htmlRenderer.ToHtml(document);
+        }
+    }
+
+    public class HtmlConverter : IHtmlConverter {}
 }
